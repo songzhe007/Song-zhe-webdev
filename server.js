138604c698cc0343eaa9d71f0287e937fcef939b
SongZheDerrick <song.zhe2@husky.neu.edu>
2017-06-19 00:08:17 -0700

    assignment3.1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/assignment'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
